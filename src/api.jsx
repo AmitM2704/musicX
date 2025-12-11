df1b023a8d33e15ffbd7b65adefd32ce01dd7324
Amit_2704 <rockmeister50@gmail.com>
2025-12-11 14:56:27 +0530

    portc

--- src/api.jsx
+++ src/api.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "https://music-x-server.onrender.com/api/admin" });
+const API = axios.create({ baseURL: import.meta.env.VITE_API_BASE_URL || "" });
 
 export default API;
