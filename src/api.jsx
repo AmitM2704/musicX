533e6537bb0de67f980506d05a3a49a1b5558444
Amit_2704 <rockmeister50@gmail.com>
2025-12-11 13:32:43 +0530

    port

--- src/api.jsx
+++ src/api.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000/api" });
+const API = axios.create({ baseURL: "https://music-x-server.onrender.com//api/admin" });
 
 export default API;
