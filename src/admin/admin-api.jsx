7424199e792e9f27b7192328d8c3a986d9649e69
Amit_2704 <rockmeister50@gmail.com>
2025-12-11 13:38:47 +0530

    port

--- src/admin/admin-api.jsx
+++ src/admin/admin-api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://music-x-server.onrender.com//api/admin"
+  baseURL: "https://music-x-server.onrender.com/api/admin"
 });
 
 // Add token to requests automatically
