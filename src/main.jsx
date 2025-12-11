ac83d30b7b24ed03501995913bc1475868954c69
Amit_2704 <rockmeister50@gmail.com>
2025-12-11 11:13:50 +0530

    redirects added

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'   // keep your existing App.js
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { MusicProvider } from './components/context/MusicCont.jsx';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
             
     
   </React.StrictMode>
